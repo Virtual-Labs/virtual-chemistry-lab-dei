a6c042136fb6aabed4bbe4b36114d2cba8cd7963
Samod Kumar <ksamod92@gmail.com>
2015-06-01 12:36:59 +0530

    Update db_config.sh
    Renamed databases file names and db names

--- scripts/db_config.sh
+++ scripts/db_config.sh
@@ -5,13 +5,13 @@ export DEBIAN_FRONTEND=noninteractive
 apt-get -q -y install mysql-server
 
 # mysql -u root -Bse "create database YOUR_DATABASE_NAME;"
-mysql -u root -Bse "create database test;"
-mysql -u root -Bse "create database local;"
+mysql -u root -Bse "create database lab_test;"
+mysql -u root -Bse "create database lab_local;"
 cd ../src/database
 
 # Replace "emt"  with your database name 
 # Replace "emt.sql" file with your file
 # Note: If you have more than one .sql file repeat below command w.r.t to database and file name
 
-mysql -u root test<test.sql
-mysql -u root local<local.sql
+mysql -u root lab_test<lab_test.sql
+mysql -u root lab_local<lab_local.sql
